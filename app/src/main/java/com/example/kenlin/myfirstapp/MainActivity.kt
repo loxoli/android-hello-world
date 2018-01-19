034dd1b26daf3539308dcf87e0f695c73e9e3ebd
loxoli <dev@chihhung.net>
2018-01-19 15:32:49 +0800

    webview

--- app/src/main/java/com/example/kenlin/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/kenlin/myfirstapp/MainActivity.kt
@@ -2,11 +2,20 @@
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webview = WebView(this)
+        setContentView(webview)
+        webview.settings.javaScriptEnabled = true
+        webview.setWebViewClient(WebViewClient())
+        webview.loadUrl("http://intranet.arbor.com.tw/")
+
     }
 }
