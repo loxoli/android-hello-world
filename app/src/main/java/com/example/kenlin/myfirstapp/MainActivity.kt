15210f96cb86db46915490ebad15ccae7abf97f8
loxoli <dev@chihhung.net>
2018-02-21 17:57:50 +0800

    override onBackPressed and change WebView to WebViewClient.

--- app/src/main/java/com/example/kenlin/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/kenlin/myfirstapp/MainActivity.kt
@@ -1,21 +1,28 @@
     package com.example.kenlin.myfirstapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+    class MainActivity : AppCompatActivity() {
+    val webViewClient = WebViewClient()
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webview = WebView(this)
-        setContentView(webview)
+        webview.webViewClient = webViewClient
         webview.settings.javaScriptEnabled = true
-        webview.setWebViewClient(WebViewClient())
         webview.loadUrl("http://intranet.arbor.com.tw/")
-
+    }
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
